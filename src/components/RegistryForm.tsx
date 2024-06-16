676489cb2635a784231525e92564058247067e3e
Nicostudent <rojo.nico@hotmail.com>
2024-06-16 12:02:03 -0300

    tally added for ceremonie and party, building

--- src/components/RegistryForm.tsx
+++ src/components/RegistryForm.tsx
@@ -1,62 +1,60 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect } from "react";
 
-export const RegistryForm = ({ party }: { party: boolean }) => {
+export const RegistryForm = ({party}:{party:boolean}) => {
   useEffect(() => {
     const widgetScriptSrc = "https://tally.so/widgets/embed.js";
 
-    const loadTallyEmbeds = () => {
+    const load = () => {
       // Load Tally embeds
       if (typeof (window as any).Tally !== "undefined") {
         (window as any).Tally.loadEmbeds();
-      } else {
-        // Fallback if window.Tally is not available
-        document
-          .querySelectorAll("iframe[data-tally-src]:not([src])")
-          .forEach((iframeEl) => {
-            (iframeEl as HTMLIFrameElement).src =
-              iframeEl.getAttribute("data-tally-src") || "";
-          });
+        return;
       }
+
+      // Fallback if window.Tally is not available
+      document
+        .querySelectorAll("iframe[data-tally-src]:not([src])")
+        .forEach((iframeEl) => {
+          (iframeEl as HTMLIFrameElement).src =
+            iframeEl.getAttribute("data-tally-src") || "";
+        });
     };
 
-    // Load Tally widgets on initial component load
-    loadTallyEmbeds();
+    // If Tally is already loaded, load the embeds
+    if (typeof (window as any).Tally !== "undefined") {
+      load();
+      return;
+    }
 
-    // Ensure Tally script is loaded if not already present
+    // If the Tally widget script is not loaded yet, load it
     if (document.querySelector(`script[src="${widgetScriptSrc}"]`) === null) {
       const script = document.createElement("script");
       script.src = widgetScriptSrc;
-      script.onload = loadTallyEmbeds; // Load embeds after script is loaded
-      script.onerror = loadTallyEmbeds; // Fallback if script loading fails
+      script.onload = load;
+      script.onerror = load;
       document.body.appendChild(script);
     }
   }, []);
 
   return (
     <div
-      className="h-screen flex justify-center items-center mb-20"
+      className="h-screen flex justify-center items-center mb-20 "
       id="Registry"
     >
-      {!party ? (
-        <iframe
-          className="flex w-11/12 md:w-1/3 flex-col"
-          data-tally-src="https://tally.so/embed/3xQ405?alignLeft=1&transparentBackground=1&dynamicHeight=1&title=Confirmar%20Asistencia"
-          loading="lazy"
-          width="100%"
-          height="495"
-          title="Confirmar Asistencia"
-        ></iframe>
-      ) : (
-        <iframe
-          className="flex w-11/12 md:w-1/3 flex-col"
-          data-tally-src="https://tally.so/embed/mOPVNA?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
-          loading="lazy"
-          width="100%"
-          height="495"
-          title="Asistencia Gastoni d12"
-        ></iframe>
-      )}
+      <iframe
+        className="flex w-11/12 md:w-1/3 flex-col"
+        data-tally-src={
+          party
+            ?"https://tally.so/embed/mOPVNA?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+            : "https://tally.so/embed/3xQ405?alignLeft=1&transparentBackground=1&dynamicHeight=1&title=Confirmar%20Asistencia"
+            
+        }
+        loading="lazy"
+        width="100%"
+        height="495"
+        title="Confirmar Asistencia"
+      ></iframe>
     </div>
   );
 };
